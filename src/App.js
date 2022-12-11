d1c697644c0f1990acbe1f9a329b6404a26e4c0a
Michael <mmm323790@gmail.com>
2022-12-11 13:50:23 +0800

    complete adding car button and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import CarModal from './components/CarModal';
+import { useState } from 'react';
+import { Button } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    const [modalOpen, setModalOpen] = useState(false)
+    const [laneNum, setLaneNum] = useState(2)
+    const [cars, setCars] = useState([[], [], [], [], []])
+
+    return (
+        <div className="App">
+            <Button onClick={() => setModalOpen(true)}>
+                Add
+            </Button>
+            <CarModal
+                open={modalOpen}
+                onCreate={({ number, time }) => {
+                    setCars(cars.map((car_line, index) => {
+                        if (index === number) {
+                            car_line.push(time)                       
+                        } 
+                        return car_line
+                    }))
+                    console.log(cars)
+                    setModalOpen(false)
+                }}
+                onCancel={() => {
+                    setModalOpen(false)
+                }}
+                laneNum={laneNum}
+            />
+            <div>
+                {cars.toString()}
+            </div>
+        </div>
+    );
 }
 
 export default App;
