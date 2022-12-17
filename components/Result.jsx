4bebc587240b877471fe97b26837ad5bf57ab5d9
Michael <mmm323790@gmail.com>
2022-12-17 11:11:31 +0800

    update choice of algorithm

---
+++ components/Result.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Space, Table, Tag } from 'antd';
+import styled from 'styled-components';
+
+
+const columns = [
+    {
+        title: 'Number',
+        dataIndex: 'number',
+        key: 'number',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Entering Time',
+        dataIndex: 'enteringTime',
+        key: 'enteringTime',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Status',
+        key: 'Status',
+        dataIndex: 'status',
+        render: (time) => {
+            let tag = "RUN"
+            let color = "pink"
+            if (time === 0) {
+                tag = "WAIT"
+                color = "red"
+            }
+            if (time < 0) {
+                tag = "ENTER"
+                color = "green"
+            }
+            return (
+                <Tag color={color} key={time}>
+                    {tag.toUpperCase()}
+                </Tag>
+            );
+
+        }
+    },
+];
+
+const Result = ({ laneIndex, enteringTime, currentTime }) => {
+    console.log(laneIndex)
+    console.log(enteringTime)
+    let lane = ["A", "B", "C", "D", "E"]
+    let data = enteringTime.map((time, idx) => {
+        let id = lane[laneIndex] + '-' + idx
+        return {
+            key: id,
+            number: id,
+            enteringTime: enteringTime[idx],
+            status: currentTime[idx],
+        }
+    })
+    return (
+        <Table columns={columns} dataSource={data} />
+    )
+}
+export default Result;
--- pages/Scene1.js
+++ pages/Scene1.js
@@ -1,17 +1,25 @@
 import CarModal from '../components/CarModal';
 import { useState, useEffect } from 'react';
-import { Space, InputNumber, Button } from 'antd';
-import { fcfs_multiple, dp, dp2 } from '../util/algorithm';
+import { Space, InputNumber, Button, Radio } from 'antd';
+import { fcfs_multiple, dp, dp2, dp_multiple } from '../util/algorithm';
 import Lane from '../components/Lane'
 import Merge from '../components/Merge';
 import Car from '../components/Car';
 import styled from 'styled-components';
+import Result from '../components/Result';
 
 const Scene1Wrapper = styled.div`
     text-align: center;
     position: relative;
 `
-
+const TableWrapper = styled.div`
+    position: absolute;
+    width: auto
+    background-color: black;
+    top: 600px;
+    display: flex;
+    justify-content: center;
+`
 
 export default function Scene1() {
     const [modalOpen, setModalOpen] = useState(false)
@@ -23,6 +31,9 @@ export default function Scene1() {
     const [savedCars, setSavedCars] = useState([[], []])
     const [result, setResult] = useState(undefined)
     const [time, setTime] = useState(0)
+    const [choice, setChoice] = useState(0)
+
+    let algorithm = [fcfs_multiple, dp_multiple]
 
     useEffect(() => {
         if (run) {
@@ -36,33 +47,36 @@ export default function Scene1() {
 
     useEffect(() => {
         // setCars(cars.map((carLine) => carLine.filter((car) => car > -25).map((car) => car - 1)))
-        setCars(cars.map((carLine, idx) => carLine.map((car, idy) => {
-            let enteringTime = result.entering_time[idx][idy]
-            if (car !== 0 || time >= enteringTime) {
-                return car - 1
-            } else {
-                return car
-            }
-        })))
+        if (time > 0) {
+            setCars(cars.map((carLine, idx) => carLine.map((car, idy) => {
+                let enteringTime = result.entering_time[idx][idy]
+                if (car !== 0 || time >= enteringTime) {
+                    return car - 1
+                } else {
+                    return car
+                }
+            })))
+        }
     }, [time])
 
     useEffect(() => {
         if (result !== undefined) {
             setSavedCars(cars)
-            console.log("saved cars")
-            console.log(cars)
+            // console.log("saved cars")
+            // console.log(cars)
+            // console.log(result.entering_time[0])
         }
     }, [result])
 
     useEffect(() => {
         console.log(result)
-        console.log(cars)
+        // console.log(cars)
     }, [result])
 
     return (
         <Scene1Wrapper>
             <Space>
-                <Button onClick={() => setModalOpen(true)}>
+                <Button onClick={() => setModalOpen(true)} disabled={result !== undefined}>
                     Add
                 </Button>
                 <Button
@@ -70,14 +84,14 @@ export default function Scene1() {
                         setLaneNum((n) => n + 1)
                         setCars([...cars, []])
                     }}
-                    disabled={laneNum === 5}
+                    disabled={laneNum === 5 || result !== undefined}
                 >Lane +</Button>
                 <Button
                     onClick={() => {
                         setLaneNum((n) => n - 1)
                         setCars(cars.slice(0, cars.length - 1))
                     }}
-                    disabled={laneNum === 2}
+                    disabled={laneNum === 2 || result !== undefined}
                 >Lane -</Button><br />
             </Space> <br />
             <Space>
@@ -124,10 +138,14 @@ export default function Scene1() {
                 }}
                 laneNum={laneNum}
             /><br />
+            <Radio.Group onChange={(e) => setChoice(e.target.value)} value={choice}>
+                <Radio value={0}>FCFS</Radio>
+                <Radio value={1}>DP</Radio>
+            </Radio.Group><br />
             <Space>
                 <Button
                     onClick={() => {
-                        setResult(fcfs_multiple(W_equal, W_plus, cars, laneNum))
+                        setResult(algorithm[choice](W_equal, W_plus, cars, laneNum))
                     }}
                     disabled={(result !== undefined)}
                 >Calculate Result</Button>
@@ -152,11 +170,13 @@ export default function Scene1() {
                     onClick={() => {
                         setRun(false)
                         setResult(undefined)
+                        setTime(0)
                         setCars(savedCars)
                     }}
                     disabled={run}
                 >Reset</Button>
             </Space><br />
+            <h3>Current Time is {time}</h3>
             <div style={{ position: 'absolute', textAlign: 'center' }}>
                 {[...Array(laneNum).keys()].map((index) => <Lane key={index} id={index} x={0} y={200 + 50 * index} width={1000} height={50} cars={cars} setCars={setCars} laneNum={laneNum} idx={index} />)}
                 <Merge x={1000} y={200 + 50 * (laneNum - 1) / 2} width={200} height={50 * laneNum} mergeHeight={50} />
@@ -168,6 +188,14 @@ export default function Scene1() {
                         if (time < 0) return <Car key={"Car" + idx + '-' + index} id={"Car" + idx + '-' + index} x={1200 - 20 * time} color={"green"} enter={true} y={200 + 50 * (laneNum - 1) / 2} />
                     }))}
             </div>
+            {(result === undefined) ? null :
+                <TableWrapper>
+                    {[...Array(laneNum).keys()].map((idx) => {
+                        return <Result laneIndex={idx} enteringTime={result.entering_time[idx]} currentTime={cars[idx]} />
+                    })}
+                </TableWrapper>
+            }
+
         </Scene1Wrapper>
     );
 }
